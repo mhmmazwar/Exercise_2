0bbb0c15c329b38e3164550704700e6b9cf1a515
aswar <mhmmdazwar@gmail.com>
2022-10-31 16:34:19 +0700

    menambahkan array

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+
+    class Program
+    {
+        
+        int[] arr = new int[20];
+
+        int n;
+        
+        int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number of the element in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements.\n");
+            }
+            
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            Console.WriteLine(" Enter array elements ");
+            Console.WriteLine("----------------------");
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
+
+       
+        }
+    }
+}
